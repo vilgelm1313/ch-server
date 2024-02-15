9c8dccf9cc177f7a01b348bf2313b6c460a3b1b0
vilgelm 1313 <vilgelm1313@gmail.com>
2024-02-15 23:56:00 +0300

    fixes

--- app/Http/Controllers/Api/Settings/ServerController.php
+++ app/Http/Controllers/Api/Settings/ServerController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 
 class ServerController extends BaseController
 {
-    protected array $syncTypes = ['channel', 'category', 'country', 'videoFile'];
+    protected array $syncTypes = ['channel', 'category', 'country', 'videofile'];
     protected function getRepositoryClass(): string
     {
         return ServerRepository::class;
