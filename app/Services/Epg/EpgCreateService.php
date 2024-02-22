481d7ce5f535d993ec969e20c14079bd93d7aea7
vilgelm 1313 <vilgelm1313@gmail.com>
2024-02-22 17:12:08 +0300

    fixes

--- app/Services/Epg/EpgCreateService.php
+++ app/Services/Epg/EpgCreateService.php
@@ -20,7 +20,7 @@ class EpgCreateService
         foreach ($channels as $channel) {
             $c = $xml->addChild('channel');
             $c->addAttribute('id', $channel->old_epg_key);
-            $name = $c->addChild('display-name', $channel->name);
+            $name = $c->addChild('display-name', htmlspecialchars(html_entity_decode($channel->name, ENT_QUOTES | ENT_XHTML, 'UTF-8')));
         }
         $programmes = Epg::where('start', '>', Carbon::now())
             ->whereHas('channel')
